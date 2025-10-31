67957da1250a83afedc293a4806b1c29aedefc9c
Renato Athaydes <renato@athaydes.com>
2025-11-01 00:13:18 +0100

    Modifying interpreter to be able to use Forth semantics

--- main.lisp
+++ main.lisp
@@ -4,18 +4,32 @@
 (in-package :forth-cl)
 
 (defmacro forth (&rest input)
+  "The main Forth macro for writing Forth in Lisp"
   `(process-from-lisp '(,@input)))
 
-(defparameter *stack* nil)
+(defparameter *stack* nil
+  "The Forth data stack")
 
-(defun no-op ())
+;; A few Forth interpreter data structures
 
 (defstruct forth-variable value)
 
 (defstruct forth-word
-  (function #'no-op :type function)
+  (function (lambda ()) :type function)
   (immediate nil    :type boolean)
   (description ""   :type string))
+
+(defparameter *forth-memory*
+  (make-array (* 10 1024) :fill-pointer 0 :adjustable t))
+
+;; execute modes are:
+;;    - immediate: fetch and immediately execute words.
+;;    - compile: fetch word, leave it in memory.
+(deftype forth-mode-t () '(member :immediate :compile))
+(declaim (type (forth-mode-t) *forth-mode*))
+(defparameter *forth-mode* :immediate)
+
+;;; Macros to help define the "code words" written in Lisp
 
 (defmacro put-word (word fun ht)
   `(setf (gethash ,word ,ht)
@@ -30,50 +44,47 @@
 (defmacro put-numop2 (sym fun ht)
   `(put-word ,sym (num-op-2 ,fun) ,ht))
 
+;;; Stack fundamental operations.
+
 (defun peek-stack ()
+  "Get the top of the stack without removing it"
   (when *stack* (car *stack*)))
 
 (defun pop-stack ()
+  "Remove the top of the stack"
   (let ((result (peek-stack)))
     (when result (setf *stack* (cdr *stack*)))
     result))
 
 (defun push-stack (item)
+  "Push item on top of the stack"
   (when item (push item *stack*)))
 
 (defun num-op-2 (op)
+  "A number operation with 2 operands that come from the stack"
   (let ((a (pop-stack))
         (b (pop-stack)))
     (if (and a b)
         (push-stack (apply op (list a b)))
-        (if b
-            (error 'missing-stack-value)
-            (error 'empty-stack)))))
+        (error 'stack-underflow))))
 
 (defun dup ()
+  "Duplicate the top element on the stack"
   (let ((a (peek-stack)))
-    (when a (push-stack a))))
+    (if a (push-stack a)
+        (error 'stack-underflow))))
 
 (defun swap ()
+  "Swap the two elements at the top of the stack"
   (let ((a (pop-stack))
         (b (pop-stack)))
-    (when (and a b)
-        (push-stack a)
-        (push-stack b))))
+    (if (and a b)
+        (progn (push-stack a) (push-stack b))
+        (error 'stack-underflow))))
 
-(defparameter *variables* (make-hash-table :test 'equal))
+;;; Definition of the Forth Dictionary
 
-(defun var-store ()
-  (let ((var (pop-stack))
-        (value (pop-stack)))
-    (if (and var value)
-        (setf (forth-variable-value var) value)
-        (error 'missing-stack-value))))
-
-(defun var-fetch ()
-  (push-stack (forth-variable-value (pop-stack))))
-
-(defparameter *words*
+(defparameter *forth-dictionary*
   (let ((ht (make-hash-table :test 'equal)))
     (put-numop2 "+" #'+ ht)
     (put-numop2 "*" #'* ht)
@@ -83,8 +94,6 @@
     (put-word! "DROP" (pop-stack) "Drops 1 stack element." nil ht)
     (put-word! "DUP" (dup) "Duplicates a stack element." nil ht)
     (put-word! "SWAP" (swap) "Swaps top 2 stack elements." nil ht)
-    (put-word "!" (var-store) ht)
-    (put-word "@" (var-fetch) ht)
     ht)
     "Forth dictionary.
    Keys are of string type. Values are of forth-word type.
@@ -93,62 +102,77 @@
    In :compile mode, it just stores the function's address in the
    word being compiled.")
 
-(defun var-define (name)
-  (let ((var (make-forth-variable)))
-    (setf (gethash name *variables*) var)
-    (put-word name (push-stack var) *words*)))
+;;; INTERPRETER
 
-(defun tick (name)
-  (let ((word (gethash name *words*)))
-    (if word
-        (push-stack word)
-        (error 'name-not-defined (format nil "undefined name: ~A~%" name)))))
+(defun push-memory (item)
+  (vector-push item *forth-memory*))
 
-(defun exec-fun (&optional word)
+(defun exec-word (&optional word)
   "Execute the given word if not nil, otherwise the one on the stack."
   (apply (forth-word-function (or word (pop-stack))) nil))
 
-;; execute modes are:
-;;    - exec: execute word
-;;    - var: define a variable
-;;    - tick: get word's function
-(defparameter *execute-mode* :exec)
+(defun coerce-to-number (n)
+  (if (numberp n) n
+      (let ((n (ignore-errors (parse-integer n))))
+        (or n (error :not-forth-value
+                     (format nil "not a Forth value: ~A~%" n))))))
 
-(defmacro restoring-exec (&rest forms)
-  `(progn ,@forms (setf *execute-mode* :exec)))
+(deftype interpreter-state-t () '(member :exec :name-compile :name-quote))
+(declaim (type (interpreter-state-t) *interpreter-state*))
+(defparameter *interpreter-state* :exec)
 
-(defun process-number (word)
-  (if (numberp word)
-      (push-stack word)
-      (let ((n (ignore-errors (parse-integer word))))
-        (if n
-            (push-stack n)
-            (error :not-forth-type (format nil "not a Forth value: ~A~%" word))))))
+(defparameter *compile-memory-start* 0)
 
-(defun process(words)
-  (dolist (word words)
-    (cond
-      ((eq *execute-mode* :var)
-       (restoring-exec (var-define word)))
-      ((eq *execute-mode* :tick)
-       (restoring-exec (tick word)))
-      ;; :exec mode
-      ((equal word "VARIABLE")
-       (setf *execute-mode* :var))
-      ((equal word "EXECUTE")
-       (exec-fun))
-      ((equal word "'")
-       (setf *execute-mode* :tick))
-      (t (let ((fw (gethash word *words*)))
-              (if fw
-                  (exec-fun fw)
-                  (process-number word)))))))
+(defun collect-words (start end)
+  "Collect the word addresses that were left in memory"
+  (loop for word-index from start to end
+        collect (aref *forth-memory* word-index)))
+
+(defun finish-compile ()
+  (let ((start *compile-memory-start*)
+        (end (fill-pointer *forth-memory*)))
+    (if (> (- end start) 0)
+        (let ((name (aref *forth-memory* start))
+              (words (collect-words (1+ start) end)))
+          (put-word name (dolist (w words)
+                           (cond ((numberp w) (push-stack w))
+                                 (t (exec-word w)))) *forth-dictionary*)))))
+
+(defun process (words)
+  "Forth interpreter."
+  (flet ((do-immediate (fw w)
+           (push-stack (or fw (coerce-to-number w))))
+         (do-compile (fw w)
+           (push-memory (or fw (coerce-to-number w)))))
+    (dolist (word words)
+      (cond
+        ((eq *interpreter-state* :name-compile)
+         (push-memory (string word))
+         (setf *interpreter-state* :exec))
+        ((eq *interpreter-state* :name-quote)
+         (push-stack (gethash (string word) *forth-dictionary*))
+         (setf *interpreter-state* :exec))
+        ((equal word "EXECUTE")
+         (exec-word))
+        ((equal word ":")
+         (setf *forth-mode* :compile)
+         (setf *interpreter-state* :name-compile)
+         (setf *compile-memory-start* (fill-pointer *forth-memory*)))
+        ((equal word ";")
+         (finish-compile))
+        ((equal word "'")
+         (setf *interpreter-state* :name-quote))
+        (t (let ((fw (gethash word *forth-dictionary*)))
+             (if (eq *forth-mode* :immediate)
+                 (do-immediate fw word)
+                 (do-compile fw word))))))))
 
 (defun process-from-lisp (&rest items)
+  "Converts items coming directly from LISP to the types expected by `process`."
   (dolist (item items)
     (cond ((listp item)
            (apply #'process-from-lisp item))
           ((symbolp item) (process (list (symbol-name item))))
-          ((numberp item) (process-number item))
+          ((numberp item) (process (list item)))
           ((stringp item) (process (list item)))
           (t (error :invalid-value (format nil "forth cannot handle value: ~A~%" item))))))
